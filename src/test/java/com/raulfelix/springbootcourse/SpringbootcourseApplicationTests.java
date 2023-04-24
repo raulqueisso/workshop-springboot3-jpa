2f86b3250dd4995b5a02c73b0fb7e5cb5193480a
Raul Felix <raulfelixsousa@gmail.com>
2023-04-24 11:56:34 -0300

    first commit

---
+++ src/test/java/com/raulfelix/springbootcourse/SpringbootcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raulfelix.springbootcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
