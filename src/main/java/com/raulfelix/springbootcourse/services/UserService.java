4f2a30d7e40983a39b67f67a360dd266370cf4fb
Raul Felix <raulfelixsousa@gmail.com>
2023-05-02 17:42:57 -0300

    Exception handling - findById

--- src/main/java/com/raulfelix/springbootcourse/services/UserService.java
+++ src/main/java/com/raulfelix/springbootcourse/services/UserService.java
@@ -2,6 +2,7 @@ package com.raulfelix.springbootcourse.services;
 
 import com.raulfelix.springbootcourse.entities.User;
 import com.raulfelix.springbootcourse.repositories.UserRepository;
+import com.raulfelix.springbootcourse.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
